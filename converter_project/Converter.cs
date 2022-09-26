0b0ee398bc43064f4ac01e5e8b37b9b149b1925a
Nikolai Kovalenko <nikolay.kov.al.006@gmail.com>
2022-09-27 01:13:20 +0300

    Делаю второе задание 2й лабы. Возникла ошибка с модификаторами доступа

---
+++ converter_project/Converter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace converter_project
+{
+    public class Converter
+    {
+        public static void Result(double inputD, double outputD, double countD)
+        {
+            double result = countD * inputD / outputD;
+            Console.WriteLine("Результат: " + result);
+        }
+
+        public static void Convert(string inputV, string outputV, double countV)
+        {
+            switch (inputV)
+            {
+                case"usd":
+                    double value1 = 39.5f;
+                    break;
+                case"eur":
+                    double value1 = 42.3f;
+                    break;
+                case"rub":
+                    double value1 = 0.62f;
+                    break;
+            }
+            
+            switch (outputV)
+            {
+                case"usd":
+                    double value2 = 39.5f;
+                    break;
+                case"eur":
+                    double value2 = 42.3f;
+                    break;
+                case"rub":
+                    double value2 = 0.62f;
+                    break;
+            }
+
+            if (countV < 0)
+            {
+                Console.WriteLine("Значение должно быть больше нуля");
+            }
+            else
+            {
+                double value3 = countV;
+            }
+            
+            Result(value1, value2, value3);
+        }
+    }
+}
--- converter_project/Program.cs
+++ converter_project/Program.cs
@@ -1,9 +1,76 @@
-﻿namespace converter_project
+﻿using System;
+
+namespace converter_project
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+
+            Convert convert = new Convert
+            {
+                
+            };
+
+            Console.WriteLine("Выбери валюту для конвертации");
+            byte i = 1;
+            while (i < 2)
+            {
+                string val = Console.ReadLine();
+
+                switch (val)
+                {
+                    case "rub":
+                        Console.WriteLine("Валюта для конветрации принята");
+                        i = 2;
+                        break;
+                    case "usd":
+                        Console.WriteLine("Валюта для конветрации принята");
+                        i = 2;
+                        break;
+                    case "eur":
+                        Console.WriteLine("Валюта для конветрации принята");
+                        i = 2;
+                        break;
+                    default: 
+                        Console.WriteLine("Нужно выбрать из предлагаемых валют: rub, eur, usd");
+                        i = 1;
+                        break;
+                }
+            }
+                        
+            Console.WriteLine("Выбери конвертируемую валюту");
+            byte j = 1;
+            while (j < 2)
+            {
+                string val1 = Console.ReadLine();
+
+                switch (val1)
+                {
+                    case "rub":
+                        Console.WriteLine("Валюта для конветрации принята");
+                        j = 2;
+                        break;
+                    case "usd":
+                        Console.WriteLine("Валюта для конветрации принята");
+                        j = 2;
+                        break;
+                    case "eur":
+                        Console.WriteLine("Валюта для конветрации принята");
+                        j = 2;
+                        break;
+                    default: 
+                        Console.WriteLine("Нужно выбрать из предлагаемых валют: rub, eur, usd");
+                        j = 1;
+                        break;
+                }
+            }
+
+            Console.WriteLine("Какое количество ?");
+            double count;
+            count = Convert.ToDouble(Console.ReadLine());
+
+            convert.Convert(i, j, count);
         }
     }
 }
